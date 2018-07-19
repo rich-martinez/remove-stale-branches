80b0aa6cf1302320f8f6351021f5923cc24163eb
Rich Martinez <richmartinez@quickenloans.com>
2018-07-18 20:37:23 -0400

    Create a script to ask the user if they want to delete a branch to leave less up to chance

---
+++ clean-branches.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#save the origin remote name
+echo "What is your origin remote name?"
+read origin
+
+readonly branches="$(git branch)"
+readonly remoteBranches=$(git ls-remote --heads origin | sed 's?.*refs/heads/??')
+
+for i in $branches
+do
+    echo $i
+    #for each branch ask user if they want to delete it locally
+    #search to see if the branch exists in the remoteBranches array
+    #if it does exist in that array ask the user if they want to delete that remote branch
+done
+
+#when done with everything let the user know
