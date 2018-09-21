805f7a7be63f142095a79dd9e1f7089ab0884c4b
Rich Martinez <richmartinez@quickenloans.com>
2018-09-21 13:28:45 -0400

    Add a note to remove wildcard from branch names

--- clean-branches.sh
+++ clean-branches.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
 #save the origin remote name
-echo "What is your origin remote name?"
+printf "What is your origin remote name?\n"
 read origin
 
 readonly branches="$(git branch)"
 readonly remoteBranches=$(git ls-remote --heads $origin | sed 's?.*refs/heads/??')
 
-echo $remoteBranches
+# filter * from beginning of branch names here
 
-for i in $branches
+#printf "\n%s\n" "$remoteBranches"
+
+printf "Choose the branches you would like to remove (e.g. 1,2,5)\n\n"
+
+branchCounter="1"
+
+for branch in $branches
 do
-    echo $i
-    #for each branch ask user if they want to delete it locally
-    #search to see if the branch exists in the remoteBranches array
-    #if it does exist in that array ask the user if they want to delete that remote branch
+    # filter the stars from the branch name before doing this.
+    ##
+    printf "%s: %s\n" "$branchCounter" "$branch"
+    branchCounter=$((branchCounter + 1))
 done
+
+#read branchesToDelete
+
+exit 0
 
 #when done with everything let the user know
