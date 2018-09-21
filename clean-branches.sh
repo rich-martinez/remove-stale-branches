128e93f0b9b74a8c624fc8d46fcf09ef44e62259
Rich Martinez <richmartinez@quickenloans.com>
2018-09-21 19:49:50 -0400

    Clean available branches and add option to remove available branches

--- clean-branches.sh
+++ clean-branches.sh
@@ -5,20 +5,25 @@ printf "What is your origin remote name?\n"
 read origin
 
 readonly branches="$(git branch)"
+readonly branchNames="${branches//[*| ]/}"
 readonly remoteBranches=$(git ls-remote --heads $origin | sed 's?.*refs/heads/??')
 
-# filter * from beginning of branch names here
+# printf "\n%s\n" "$branchesCleaned"
+# printf "\n%s\n" "$remoteBranches"
 
-#printf "\n%s\n" "$remoteBranches"
+printf "\nDo you want to remove all the branches: (y/N)\n"
+read selectedOption
+readonly selectedOptionLowerCase="$(echo $selectedOption|tr '[:upper:]' '[:lower:]')"
 
-printf "Choose the branches you would like to remove (e.g. 1,2,5)\n\n"
+if [ "$selectedOptionLowerCase" = "n" ]; then
+    printf "\nPlease choose the branches you would like to remove.\n"
+    printf "\nPossible Options:\n"
+fi
 
 branchCounter="1"
 
-for branch in $branches
+for branch in $branchNames
 do
-    # filter the stars from the branch name before doing this.
-    ##
     printf "%s: %s\n" "$branchCounter" "$branch"
     branchCounter=$((branchCounter + 1))
 done
