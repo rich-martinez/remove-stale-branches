70f0df41a27023f1a24750638b0cb9e3afdf17f5
Rich Martinez <richmartinez@quickenloans.com>
2018-09-28 21:36:17 -0400

    Add a function to filter/remove branches by name

--- clean-branches.sh
+++ clean-branches.sh
@@ -8,8 +8,9 @@ printf "What is your origin remote name?\n"
 read origin
 
 readonly branches="$(git branch)"
-readonly branchNames="${branches//[*| ]/}"
+declare branchNames="${branches//[*| ]/}"
 readonly remoteBranches=$(git ls-remote --heads $origin | sed 's?.*refs/heads/??')
+declare remoteBrancheNames="${remoteBranches//[*| ]/}"
 
 printAvailableBranches() {
   printf "\nAvailable Branches:\n"
@@ -43,40 +44,63 @@ showOptions() {
 showOptions
 read selectedOption
 
-readonly sanitizedSelectedOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')"
-delcare sanitizedSelectedOptionList=""
-# Did the user provide a list?
-if [ ${selectedOption:${#selectedOption}-1} = "]" ]; then
-  # remove suffix of '['
-  sanitizedSelectedOptionList="${selectedOption//']'/}"
+readonly sanitizedSelectedOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')|tr ',' '[:space:]')"
 
-  # Is it a list of branches to keep?
-  if [ "${selectedOption:0:2}" = "!["  ]; then
-    # remove prefixe from list
-     sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'!['/}"
+filterBranchNames() {
+  # Did the user provide a list?
+  if [ ${sanitizedSelectedOption:${#sanitizedSelectedOption}-1} = "]" ]; then
+    # remove suffix of '['
+    sanitizedSelectedOptionList="${sanitizedSelectedOption//']'/}"
 
-  # Is it a list of branches to remove?
-  elif [ "${selectedOption:0:1}" = "["  ]; then
-    # remove prefix from list
-     sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'['/}"
+    # Is it a list of branches to keep?
+    if [ "${sanitizedSelectedOption:0:2}" = "!["  ]; then
+      # remove prefixes from list
+      sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'!['/}"
+      for branchNumber in sanitizedSelectedOptionList
+      do
+        # remove current item from the branchNames array
+      done
+
+    # Is it a list of branches to remove?
+    elif [ "${selectedOption:0:1}" = "["  ]; then
+      # remove prefix from list
+      sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'['/}"
+      for branchName in branchNames
+      do
+        # if the current branch name is not in the sanitizedSelectedOptionList then remove it from the
+        # branchNames list
+      done
+    fi
+
+    # If the list is not prefixed with '![' or '[' then provide an error message and rerun the prompt
+    unexpectedOption selectedOption
+  elif [ "$sanitizedSelectedOption" = "all" ]; then
+    printf "All branches will be removed.\n"
+  elif [ "$sanitizedSelectedOption" = "none" ]; then
+    # remove all branches from the branchNames array
+    branchNames=""
+  elif [ "$sanitizedSelectedOption" = "!$mainBranch" ]; then
+    # remove the $mainBranch from $branchNames and then delete the remain branches using git.
+  else
+    printf "%s is not an available option.\n" "$selectedOption"
+    exit 1
   fi
+}
 
-  # If the list is not prefixed with '![' or '[' then provide an error message and rerun the prompt
-  unexpectedOption selectedOption
-elif [ "$selectedOption" = "all" ]; then
-  for branch in $branchNames
+
+
+removeSelectedBranches() {
+  filterBranchNames
+
+  for branch in branchNames
   do
-    $(git branch -D $branch)
-    branchCounter=$((branchCounter + 1))
+    # git branch -D "$branch"
   done
-elif [ "$selectedOption" = "none" ]; then
-  printf "Continuing...\n"
-elif [ "$selectedOption" = "!$mainBranch" ]; then
-  # remove the $mainBranch from $branchNames and then delete the remain branches using git.
-else
-  printf "%s is not an available option.\n" "$selectedOption"
-  exit 1
-fi
+}
+
+removeSelectedBranches
+
+printf "Continuing to remote branches...\n"
 
 #read branchesToDelete
 
