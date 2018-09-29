dff3936e09d835e0ff22996e8730be954df92d5a
Rich Martinez <richmartinez@quickenloans.com>
2018-09-29 13:14:22 -0400

    Add function to check for git and add prompt for removal types

--- clean-branches.sh
+++ clean-branches.sh
@@ -1,13 +1,58 @@
 #!/usr/bin/env bash
 
-# This is meant to optionally store the main branch that everything else is based off of (e.g. master)
-readonly mainBranch="${1:-master}"
 readonly branches="$(git branch)"
 declare -a branchNames="${branches//[*| ]/}"
+declare -a branchesAvailableForRemoval="${branchNames[@]/mainBranch/}"
+
+checkForGit() {
+  if [[ $(which git &> /dev/null) ]] && [[ $? -ne 0 ]]; then
+      printf "Please make sure git is installed and available to use on the command line before running this script.\n"
+      exit 1
+  fi
+}
+
+checkoutTheMainBranch() {
+  printf "Please choose the main branch which will not be removed (e.g. master).\n"
+  read mainBranch
+
+  # Check that main branch is a branch in this repository
+  if [[ ! "${branchNames[@]}" =~ "$mainBranch" ]]; then
+      printf "%s is not a branch for this repository." "$mainBranch"
+      exit 1
+  fi
+
+  printf "Moving to the main branch (i.e. %s). It will not be removed.\n" "$mainBranch"
+  git checkout "$mainBranch"
+}
+
+printAvailableRemovalTypes() {
+  printf "\nAvailable Branch Removal Types:\n"
+  declare -a branchRemovalTypes=(
+    "Remove local branches."
+    "Remove remote branches"
+    "Prune remotes."
+  )
+
+  for ((i = 0; i < ${#branchRemovalTypes[@]}; i++))
+  do
+    printf "    %s: %s\n" "$((i+1))" "${branchRemovalTypes[$i]}"
+    counter=$((counter + 1))
+  done
+}
+
+getBranchRemovalTypes() {
+  printAvailableRemovalTypes
+
+  printf "\nPlease choose what to do from the options above: (1,2,3)"
+  read  branchRemovalTypes
+}
+
+getBranchRemovalTypes
+exit 0
 
 printAvailableBranches() {
   printf "\nBranches Available for Removal:\n"
-  theBranchNames="$1"
+  declare -a theBranchNames="$1"
   branchCounter="1"
   for branch in $theBranchNames
   do
@@ -20,16 +65,14 @@ printBranchRemovalOptions() {
   printf "\nAvailable Options:
     [associated branch number(s)] - This is a brackets list/array populated with branches to be removed by associated branch number. Example: [1,2,5]\n
     ![associated branch number(s)] - This is a brackets list/array, preceded by and exclaimation point, populated with branches to skip removing by associated branch number. Example: ![1,2,5]
-    none - This will select no branches to be removed.\n
     all - This will select all the branches to be removed.\n
-    !%s - Do not remove the %s branch but remove all the other branches.\n
-  " "$mainBranch" "$mainBranch"
+  "
 }
 
 showOptions() {
   declare -a availableBranchNames="($1)"
-  if [ "${#availableBranchNames[@]}" -eq "1" ]; then
-    printf "'%s' can not be removed because it is the only local branch\n" "${availableBranchNames[0]}"
+  if [ "${#availableBranchNames[@]}" -eq "0" ]; then
+    printf "No local branches can be removed because there is only one local branch\n"
     exit 1
   fi
 
@@ -38,12 +81,17 @@ showOptions() {
   printf "\nPlease choose which of the branches to remove from list above: (!%s)\n" "$mainBranch"
 }
 
-showOptions "$branchNames"
+#####################
+# Start prompting the user here
+#####################
+getBranchRemovalTypes
+checkForGit
+showOptions "$branchesAvailableForRemoval"
 read selectedOption
 
-readonly sanitizedSelectedOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')|tr ',' '[:space:]')"
-
 filterBranchNames() {
+  readonly sanitizedSelectedOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')|tr ',' '[:space:]')"
+
   # Did the user provide a list?
   if [ ${sanitizedSelectedOption:${#sanitizedSelectedOption}-1} = "]" ]; then
     # remove suffix of '['
@@ -68,27 +116,23 @@ filterBranchNames() {
         # branchNames list
       done
     fi
-
     # If the list is not prefixed with '![' or '[' then provide an error message and rerun the prompt
     unexpectedOption selectedOption
   elif [ "$sanitizedSelectedOption" = "all" ]; then
     printf "All branches will be removed.\n"
-  elif [ "$sanitizedSelectedOption" = "none" ]; then
-    # remove all branches from the branchNames array
-    branchNames=""
-  elif [ "$sanitizedSelectedOption" = "!$mainBranch" ]; then
-    # remove the $mainBranch from $branchNames and then delete the remain branches using git.
-  else
+  fi
+
     printf "%s is not an available option.\n" "$selectedOption"
     exit 1
-  fi
 }
 
 removeSelectedBranches() {
+  checkoutTheMainBranch
+
   # pass in an argument of the type of branches (e.g. remote branches, local branches) to filterBranchNames
   filterBranchNames
 
-  for branch in $branchNames
+  for branch in $branchesAvailableForRemoval
   do
     # git branch -D "$branch"
   done
@@ -99,7 +143,7 @@ removeSelectedBranches
 printf "Continuing to remote branches...\n"
 
 askForRemoteRepository() {
-  printf "Do you have an associated remote repository?(Y\n)\n"
+  printf "Do you have an associated remote repository?: (Y\n)\n"
   read hasRemote
   readonly sanitizedHasRemoteOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]')"
   case "$hasRemote" in
@@ -107,6 +151,7 @@ askForRemoteRepository() {
       #save the origin remote name
       printf "What is your origin remote name?\n"
       read originRemote
+      #TODO: Make sure the remote exists in this repository before getting remote branches
       readonly remoteBranches="$(git ls-remote --heads $originRemote | sed 's?.*refs/heads/??')"
       declare remoteBranchNames="${remoteBranches//[*| ]/}"
       showOptions "$remoteBranchNames"
