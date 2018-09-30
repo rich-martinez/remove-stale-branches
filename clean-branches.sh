87ac6abf2af66594f5544487b723ce59552a112e
Rich Martinez <richmartinez@quickenloans.com>
2018-09-29 21:08:09 -0400

    Add runLocalBranchRemoval function

--- clean-branches.sh
+++ clean-branches.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-readonly branches="$(git branch)"
-declare -a branchNames="${branches//[*| ]/}"
-declare -a branchesAvailableForRemoval="${branchNames[@]/mainBranch/}"
-
 checkForGit() {
   if [[ $(which git &> /dev/null) ]] && [[ $? -ne 0 ]]; then
       printf "Please make sure git is installed and available to use on the command line before running this script.\n"
@@ -11,48 +7,25 @@ checkForGit() {
   fi
 }
 
-checkoutTheMainBranch() {
-  printf "Please choose the main branch which will not be removed (e.g. master).\n"
-  read mainBranch
+checkExistenceOfMainBranch() {
+  readonly mainBranch="$1"
+  readonly branchNames="$2"
 
   # Check that main branch is a branch in this repository
   if [[ ! "${branchNames[@]}" =~ "$mainBranch" ]]; then
       printf "%s is not a branch for this repository." "$mainBranch"
       exit 1
   fi
+}
 
+checkoutTheMainBranch() {
   printf "Moving to the main branch (i.e. %s). It will not be removed.\n" "$mainBranch"
   git checkout "$mainBranch"
 }
 
-printAvailableRemovalTypes() {
-  printf "\nAvailable Branch Removal Types:\n"
-  declare -a branchRemovalTypes=(
-    "Remove local branches."
-    "Remove remote branches"
-    "Prune remotes."
-  )
-
-  for ((i = 0; i < ${#branchRemovalTypes[@]}; i++))
-  do
-    printf "    %s: %s\n" "$((i+1))" "${branchRemovalTypes[$i]}"
-    counter=$((counter + 1))
-  done
-}
-
-getBranchRemovalTypes() {
-  printAvailableRemovalTypes
-
-  printf "\nPlease choose what to do from the options above: (1,2,3)"
-  read  branchRemovalTypes
-}
-
-getBranchRemovalTypes
-exit 0
-
 printAvailableBranches() {
   printf "\nBranches Available for Removal:\n"
-  declare -a theBranchNames="$1"
+  declare -a theBranchNames="($1)"
   branchCounter="1"
   for branch in $theBranchNames
   do
@@ -78,16 +51,7 @@ showOptions() {
 
   printAvailableBranches "$availableBranchNames"
   printBranchRemovalOptions
-  printf "\nPlease choose which of the branches to remove from list above: (!%s)\n" "$mainBranch"
 }
-
-#####################
-# Start prompting the user here
-#####################
-getBranchRemovalTypes
-checkForGit
-showOptions "$branchesAvailableForRemoval"
-read selectedOption
 
 filterBranchNames() {
   readonly sanitizedSelectedOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')|tr ',' '[:space:]')"
@@ -101,19 +65,21 @@ filterBranchNames() {
     if [ "${sanitizedSelectedOption:0:2}" = "!["  ]; then
       # remove prefixes from list
       sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'!['/}"
-      for branchNumber in sanitizedSelectedOptionList
+      for branchNumber in $sanitizedSelectedOptionList;
       do
         # remove current item from the branchNames array
+        printf "Pretend removing current branch from the list of available branch names.\n"
       done
 
     # Is it a list of branches to remove?
     elif [ "${selectedOption:0:1}" = "["  ]; then
       # remove prefix from list
       sanitizedSelectedOptionList="${sanitizedSelectedOptionList//'['/}"
-      for branchName in $branchNames
+      for branchName in $branchNames;
       do
         # if the current branch name is not in the sanitizedSelectedOptionList then remove it from the
         # branchNames list
+        printf "Pretend filter of current branch name from list.\n"
       done
     fi
     # If the list is not prefixed with '![' or '[' then provide an error message and rerun the prompt
@@ -129,23 +95,50 @@ filterBranchNames() {
 removeSelectedBranches() {
   checkoutTheMainBranch
 
-  # pass in an argument of the type of branches (e.g. remote branches, local branches) to filterBranchNames
-  filterBranchNames
-
   for branch in $branchesAvailableForRemoval
   do
-    # git branch -D "$branch"
+    printf "Pretend removing %s" "$branch"
   done
 }
 
-removeSelectedBranches
+runLocalBranchRemoval() {
+  checkForGit
 
-printf "Continuing to remote branches...\n"
+  printf "Please choose the main branch which will not be removed: (master)"
+  read mainBranch
+  declare sanitizedMainBranch="$(echo $mainBranch|tr -d '[:space:]')"
+  if [ -z "$sanitizedMainBranch" ]; then
+    sanitizedMainBranch="master"
+  fi
+
+  readonly branches="$(git branch)"
+  declare -a branchNames="${branches//[*| ]/}"
+
+  checkExistenceOfMainBranch "$sanitizedMainBranch" "$branchNames"
+
+  declare -a branchesAvailableForRemoval="$(echo $branchNames|sed 's/\'$sanitizedMainBranch'//')"
+
+  printf "\nDo you want to remove local branches: (Y/n)"
+  read  -n 1 removeLocalBranches
+  readonly sanitizedLocalBranchRemovalOption="$(echo $removeLocalBranches|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')"
+  if [ -z "$sanitizedLocalBranchRemovalOption" ] || [ "$sanitizedLocalBranchRemovalOption" = "y" ]; then
+    showOptions "$branchesAvailableForRemoval"
+    printf "\nPlease choose which of the branches to remove from list above: (all)"
+    read selectedOption
+
+    filterBranchNames
+
+    removeSelectedBranches
+  else
+    # Don't do anything with local branches and go to next prompt.
+    return
+  fi
+}
 
 askForRemoteRepository() {
   printf "Do you have an associated remote repository?: (Y\n)\n"
   read hasRemote
-  readonly sanitizedHasRemoteOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]')"
+  readonly sanitizedHasRemoteOption="$(echo $selectedOption|tr '[:upper:]' '[:lower:]'|tr -d '[:space:]')"
   case "$hasRemote" in
     y)
       #save the origin remote name
@@ -155,17 +148,20 @@ askForRemoteRepository() {
       readonly remoteBranches="$(git ls-remote --heads $originRemote | sed 's?.*refs/heads/??')"
       declare remoteBranchNames="${remoteBranches//[*| ]/}"
       showOptions "$remoteBranchNames"
+      ;;
     n)
       printf "Skipping removal of any remote branches."
+      ;;
     *)
       printf "%s is not in available option" "$hasRemote"
       ;;
+  esac
 }
 
-askForRemoteRepository
-
-
-#read branchesToDelete
+#####################
+# Start prompting the user here
+#####################
+runLocalBranchRemoval
 
 exit 0
 
