b4c6828056dd58fb027cff78b5d9a8e3e673cd71
Rich Martinez <richmartinez@quickenloans.com>
2018-11-09 22:31:19 -0500

    Add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -40,7 +40,14 @@ module.exports = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: null,
+  coverageThreshold: {
+    "global": {
+      "branches": 95,
+      "functions": 95,
+      "lines": 95,
+      "statements": 95
+    },
+  },
 
   // Make calling deprecated APIs throw helpful error messages
   // errorOnDeprecated: false,
