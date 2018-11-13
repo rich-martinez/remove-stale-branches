f80cc675260936194324708218024d48dc49c963
Rich Martinez <richmartinez@quickenloans.com>
2018-11-12 21:49:00 -0500

    Use content keys for staleness types

--- src/removeStaleBranches.js
+++ src/removeStaleBranches.js
@@ -4,6 +4,7 @@ const removeStaleBranches = async () => {
     const { stalenessRemovalOptionsPrompt } = require('./main/prompts/stalenessRemovalOptions/stalenessRemovalOptionsPrompt');
     const { removeStalenessContinuationPrompt } = require('./main/prompts/removeStalenessContinuation/removeStalenessContinuationPrompt');
     const { runLocalBranchRemoval } = require('./local-branches/localBranchRemoval');
+    const { removeLocalBranches, removeRemoteBranches } = require('./main/prompts/stalenessRemovalOptions/stalenessRemovalOptionsContent');
 
     let removeStalenessContinuationAnswer = true;
     let previouslyRemovedBranches = {
@@ -14,10 +15,11 @@ const removeStaleBranches = async () => {
     // make this a loop and make the responses to each removal option available to every other removal option
     while (removeStalenessContinuationAnswer === true) {
         const stalenessRemovalOptionsAnswer =  await stalenessRemovalOptionsPrompt();
-        if (stalenessRemovalOptionsAnswer === 'removeLocalBranches') {
+
+        if (stalenessRemovalOptionsAnswer === removeLocalBranches) {
             const removedLocalBranches = await runLocalBranchRemoval(previouslyRemovedBranches);
             previouslyRemovedBranches.localBranches.concat(removedLocalBranches);
-        } else if (stalenessRemovalOptionsAnswer === 'removeRemoteBranches') {
+        } else if (stalenessRemovalOptionsAnswer === removeRemoteBranches) {
             // await runRemoteBranchRemoval(previouslyRemovedBranches);
         }
         removeStalenessContinuationAnswer = await removeStalenessContinuationPrompt();
