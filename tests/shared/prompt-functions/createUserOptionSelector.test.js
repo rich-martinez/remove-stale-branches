b491b1e4569ebe9eb541447590aa8beba6d93c61
Rich Martinez <richmartinez@quickenloans.com>
2018-12-07 22:19:23 -0500

    Add expectation for createUserOptionSelector return value

--- tests/shared/prompt-functions/createUserOptionSelector.test.js
+++ tests/shared/prompt-functions/createUserOptionSelector.test.js
@@ -1,4 +1,7 @@
 const { createUserOptionSelector } = require('../../../src/shared/prompt-functions/createUserOptionSelector');
+const { fuzzyUserOptionSearch } = require('../../../src/shared/fuzzyUserOptionSearch');
+
+jest.mock('../../../src/shared/fuzzyUserOptionSearch');
 
 test('Calling createUserOptionSelector creates expected async function', async () => {
   const options = ['user option 1', 'user option 2'];
@@ -7,8 +10,13 @@ test('Calling createUserOptionSelector creates expected async function', async (
 
     return optionSearch;
   }`;
+  const expectedFuzzySearchResult = 'fuzzy filtered value';
+  fuzzyUserOptionSearch.mockReturnValue(Promise.resolve(expectedFuzzySearchResult));
+
   const userOptionSelector = createUserOptionSelector(options);
+  const fuzzySearch = await userOptionSelector(undefined, 'some value');
 
   expect(typeof userOptionSelector).toBe('function');
   expect(userOptionSelector.toString()).toBe(expectedReturnValue);
+  expect(fuzzySearch).toBe(expectedFuzzySearchResult);
 });
