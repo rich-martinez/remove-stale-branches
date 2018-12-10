a7608353d50e8c4d85f13576532061b24d88039c
Rich Martinez <richmartinez@quickenloans.com>
2018-12-10 15:49:03 -0500

    Add test for branchesToRemove user keep branches and add test for anything else

--- tests/shared/branchesToRemove.test.js
+++ tests/shared/branchesToRemove.test.js
@@ -12,6 +12,8 @@ jest.mock('../../src/shared/prompts/branchRemovalStrategyPrompt');
 jest.mock('../../src/shared/prompts/removeSelectedBranchesPrompt');
 jest.mock('../../src/shared/prompts/keepSelectedBranchesPrompt');
 
+global.console = {log: jest.fn()};
+
 test('All branches available for removal will be removed', async () => {
   const branchesAvailableForRemoval = ['branch1', 'branch2', 'branch3', 'branch4'];
   branchRemovalStrategyPrompt.mockReturnValue(removeAllAvailableBranchesContent);
@@ -55,4 +57,54 @@ test('Only user selected branches will be removed.', async () => {
   expect(keepSelectedBranchesPrompt).toBeCalledTimes(0);
   expect(removeSelectedBranchesPrompt).toBeCalledTimes(1);
   expect(removeSelectedBranchesPrompt).toBeCalledWith(branchesAvailableForRemoval);
+});
+
+test('User selected branches will not be removed but all other branches will be removed.', async () => {
+  const branchesAvailableForRemoval = ['branch1', 'branch2', 'branch3', 'branch4'];
+  const branchesToBeRemoved = ['branch1', 'branch4'];
+  branchRemovalStrategyPrompt.mockReturnValue(keepSelectedBranchesContent);
+  keepSelectedBranchesPrompt.mockReturnValue(branchesToBeRemoved);
+
+  const theBranchesToRemove = await branchesToRemove(branchesAvailableForRemoval);
+
+  expect(branchRemovalStrategyPrompt).toBeCalledTimes(1);
+  expect(branchRemovalStrategyPrompt)
+    .toBeCalledWith(
+      branchesAvailableForRemoval,
+      [
+        removeAllAvailableBranchesContent,
+        removeSelectedBranchesContent,
+        keepSelectedBranchesContent,
+      ],
+    )
+  expect(theBranchesToRemove).toEqual(branchesToBeRemoved);
+  expect(removeSelectedBranchesPrompt).toBeCalledTimes(0);
+  expect(keepSelectedBranchesPrompt).toBeCalledTimes(1);
+  expect(keepSelectedBranchesPrompt).toBeCalledWith(branchesAvailableForRemoval);
+});
+
+test('Unexpected branch removal strategy value leads to an empy array being returned.', async () => {
+  const branchesAvailableForRemoval = ['branch1', 'branch2', 'branch3', 'branch4'];
+  const branchesToBeRemoved = ['branch1', 'branch4'];
+  const unexpectedValue = 'some unexpected value';
+  branchRemovalStrategyPrompt.mockReturnValue(unexpectedValue);
+
+  const theBranchesToRemove = await branchesToRemove(branchesAvailableForRemoval);
+
+
+  expect(branchRemovalStrategyPrompt).toBeCalledTimes(1);
+  expect(branchRemovalStrategyPrompt)
+    .toBeCalledWith(
+      branchesAvailableForRemoval,
+      [
+        removeAllAvailableBranchesContent,
+        removeSelectedBranchesContent,
+        keepSelectedBranchesContent,
+      ],
+    )
+    expect(theBranchesToRemove).toEqual([]);
+    expect(removeSelectedBranchesPrompt).toBeCalledTimes(0);
+    expect(keepSelectedBranchesPrompt).toBeCalledTimes(0);
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith('No branches will be removed');
 });
