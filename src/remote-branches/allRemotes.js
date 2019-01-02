89b26f704bc18dd707a19ace5e5927e96316f4e1
Rich Martinez <richmartinez@quickenloans.com>
2019-01-01 21:15:10 -0500

    Add test for allRemotes

--- src/remote-branches/allRemotes.js
+++ src/remote-branches/allRemotes.js
@@ -1,8 +1,8 @@
-const simpleGit = require('simple-git/promise')()
+const simpleGit = require('simple-git/promise')
 
 /**
  * @returns {Promise}
  */
 exports.allRemotes = async () => {
-  return simpleGit.getRemotes().then(remotes => remotes.map(remote => remote.name))
+  return simpleGit().getRemotes().then(remotes => remotes.map(remote => remote.name))
 }
---
+++ tests/remote-branches/allRemotes.test.js
@@ -0,0 +1,24 @@
+const simpleGit = require('simple-git/promise');
+const { allRemotes } = require('../../src/remote-branches/allRemotes');
+
+jest.mock('simple-git/promise');
+
+test('No remotes exist and it is resolved to an empty array', async () => {
+  const expectedResult = [];
+  simpleGit.mockReturnValue({ getRemotes: () => Promise.resolve(expectedResult) });
+
+  const remotes = await allRemotes();
+
+  expect(simpleGit).toBeCalledTimes(1);
+  expect(remotes).toEqual(expectedResult);
+});
+
+test('Available remotes exist and they resolve to an array', async () => {
+  const expectedResult = ['remote1', 'remote2'];
+  simpleGit.mockReturnValue({ getRemotes: () => Promise.resolve([{name: 'remote1'}, {name: 'remote2'}]) });
+
+  const remotes = await allRemotes();
+
+  expect(simpleGit).toBeCalledTimes(1);
+  expect(remotes).toEqual(expectedResult);
+});
