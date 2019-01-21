95f1265b3b0bbc6b7daf2d9e78984f76d4204b28
Rich Martinez <richmartinez@quickenloans.com>
2019-01-21 17:00:08 -0500

    Add bin config in package.json and move entry.js to the bin directory

--- bin/entry.js
+++ bin/entry.js
@@ -3,6 +3,6 @@
 /**
  * This is the entry point for removing stale remote/local branches.
  */
-const { removeStaleBranches } = require('./src/core/removeStaleBranches')
+const { removeStaleBranches } = require('../src/core/removeStaleBranches')
 
 removeStaleBranches()
